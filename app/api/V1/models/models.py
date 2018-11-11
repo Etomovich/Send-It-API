d7532f5bb2fd99637fcd385597f19a89aa2ca3b1
BrianSerem <44457835+BrianSerem@users.noreply.github.com>
2018-11-11 23:19:37 +0300

    Update models.py

--- app/api/V1/models/models.py
+++ app/api/V1/models/models.py
@@ -4,7 +4,7 @@ destinations = ['nairobi', 'nakuru', 'kisumu']
 
 class User():
     def __init__(self,id,username,password):
-        self.id = id
+        self.id = __id
         self.username = username
         self.password = password
 
