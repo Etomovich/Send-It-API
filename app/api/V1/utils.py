67b3b83b523ee120bd35fb81200160513e601646
BrianSerem <brianseremiat@gmail.com>
2018-11-20 22:42:55 +0300

    [Chore #162042923] Worked on feedback.

--- app/api/V1/utils.py
+++ app/api/V1/utils.py
@@ -2,31 +2,24 @@ import re
 
 
 def valid_destination_name(destination):
-    ''' validate destination name '''
+    """Test destination name if valid."""
     regex = "^[a-zA-Z0-9_ ]{1,}$"
     return re.match(regex, destination)
 
 
 def valid_origin_name(origin):
-    ''' validate origin name '''
+    """Test origin name."""
     regex = "^[a-zA-Z0-9_ ]{5,}$"
     return re.match(regex, origin)
 
 
 def valid_person_name(customer_name):
-    '''validate person's name'''
+    """Test person name."""
     regex = "^[a-zA-Z ]{4,}$"
     return re.match(regex, customer_name) 
 
 
 def validate_input(input):
-    '''validate input'''
+    """Validate input."""
     regex = "^[a-zA-Z0-9_]{1,20}$"
     return re.match(regex,input)   
-
-
-    
-def valid_destination(destination):
-    '''validate destination name'''
-    regex = "^[a-zA-Z 0-9]{3,}$"
-    return re.match(regex, destination)
