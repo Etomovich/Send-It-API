77ae285ab4131182fff941c17605c2eab3edbb52
BrianSerem <44457835+BrianSerem@users.noreply.github.com>
2018-11-20 15:41:33 +0300

    Update test.py

--- app/tests/V1/test.py
+++ app/tests/V1/test.py
@@ -180,7 +180,7 @@ class TestAllOrders(unittest.TestCase):
             "api/v1/parcels/1/declined", data=json.dumps(self.data),
             headers={"content-type": "application/json"}
         )
-        self.assertEqual(res.status_code, 200)
+        self.assertEqual(res.status_code, 400)
 
     def test_invalid_origin_name(self):
         """Test invalid origin name."""
