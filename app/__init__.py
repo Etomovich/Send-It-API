374cd43e9e5673cb85622ac9cc96481492ee7f34
BrianSerem <brianseremiat@gmail.com>
2018-11-21 05:36:17 +0300

    [Feature #161996678] Added a user sign up and database storage.

--- app/__init__.py
+++ app/__init__.py
@@ -3,14 +3,17 @@ from flask import Flask
 from .api.V1 import version_1
 from .api.V2 import version_2
 from app import db_config
+from flask_jwt_extended import JWTManager
 
 
 def create_app():
     """Crete app fuction."""
     app = Flask(__name__)
+    app.secret_key = "brian"
     db_config.create_orders_table()
     db_config.create_users_table()
     app.register_blueprint(version_1)
     app.register_blueprint(version_2)
+    jwt = JWTManager(app)
 
     return app
