89a857be66f22eea3776e3db894862050b83b4a6
BrianSerem <brianseremiat@gmail.com>
2018-11-22 10:31:11 +0300

    chore(admin change parcel status):remove extra lines in the code files -remove extra empty lines in code files
    [Finishes #162140058]

--- app/api/V2/__init__.py
+++ app/api/V2/__init__.py
@@ -1,11 +1,11 @@
 """Contain urls and blueprint."""
 from flask import Blueprint
 from flask_restful import Api
-from .views.parcelviews import GetParcels, CreateParcel
+from .views.parcelviews import GetParcels, CreateParcel, ChangeCurrentLocation
 from .views.parcelviews import GetUserParcels,GetSpecificParcel
 from .views.parcelviews import ChangeParcelDestination, ChangeParcelStatus
 from .views.userviews import UserRegistration, TokenRefresh
-from .views.userviews import GetAllUsers, UserLogin 
+from .views.userviews import GetAllUsers, UserLogin, GetSpecificUser
 
 version_2 = Blueprint('v2', __name__)
 
@@ -15,9 +15,11 @@ api.add_resource(GetSpecificParcel, '/parcels/<int:order_id>')
 api.add_resource(CreateParcel, '/parcels')
 api.add_resource(GetParcels, '/parcels')
 api.add_resource(GetUserParcels, '/users/<int:user_id>/parcels')
+api.add_resource(GetSpecificUser, '/users/<int:user_id')
 api.add_resource(UserRegistration, '/auth/signup')
 api.add_resource(UserLogin, '/auth/login')
 api.add_resource(TokenRefresh, '/auth')
 api.add_resource(GetAllUsers, '/users')
 api.add_resource(ChangeParcelDestination, '/parcels/<int:order_id>/destination')
 api.add_resource(ChangeParcelStatus, 'parcels/<int:order_id>/status')
+api.add_resource(ChangeCurrentLocation, 'parcels/<int:order_id>/parcelocation')
--- app/api/V2/models/parcelmodels.py
+++ app/api/V2/models/parcelmodels.py
@@ -47,5 +47,3 @@ class Order:
         else:
             order = None
         return order
-
-    
