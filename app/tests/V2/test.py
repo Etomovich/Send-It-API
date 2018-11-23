83353817f7454e9140b46ba1d181066e2cb4f956
BrianSerem <brianseremiat@gmail.com>
2018-11-23 15:37:27 +0300

    [Chore #162154359] Added a test base case.

--- app/tests/V2/test.py
+++ app/tests/V2/test.py
@@ -34,6 +34,29 @@ class TestDeliveryOrders(unittest.TestCase):
             "current location": "kikuyu",
     
         }
+        self.client.post(
+            '/api/v2/auth/signup', data=json.dumps(self.signup_data),
+            content_type='application/json')
+        res = self.client.post(
+            '/api/v2/auth/login', data=json.dumps(self.login_data),
+            content_type='application/json')
+        data = json.loads(res.get_data(as_text=True))
+        self.token = data.get('access_token')
+        self.user_headers = {
+            'AUTHORIZATION': 'Bearer ' + self.token
+        }
+        self.client.post(
+            '/api/v2/auth/signup', data=json.dumps(self.signup_admin),
+            content_type='application/json')
+        res = self.client.post(
+            '/api/v2/auth/login', data=json.dumps(self.login_admin),
+            content_type='application/json')
+        data = json.loads(res.get_data(as_text=True))
+        self.token = data.get('access_token')
+        self.admin_headers = {
+            'AUTHORIZATION': 'Bearer ' + self.token
+        }
+
 
         self.headers = {}
 
@@ -263,6 +286,64 @@ class TestDeliveryOrders(unittest.TestCase):
 
         result = json.loads(response.data)
         self.assertIn('in-transit', str(result))
+
+    def test_create_user(self):
+        """Docstring for test_create_user method."""
+        res = self.client.post(
+            '/api/v2/auth/signup',
+            data=json.dumps(self.signup_to_create_parcel_order),
+            content_type='application/json')
+
+        self.assertEqual(res.status_code, 201)
+
+    def test_user_not_created(self):
+        """Docstring for test_user_not_found method."""
+        self.client.post(
+            '/api/v2/auth/signup', data=json.dumps(self.signup_data),
+            content_type='application/json')
+        res2 = self.client.post(
+            '/api/v2/auth/signup', data=json.dumps(self.signup_data),
+            content_type='application/json')
+        result = json.loads(res2.data)
+        self.assertEqual(result["message"], "user already exists")
+        self.assertEqual(res2.status_code, 409)
+
+    def test_user_login(self):
+        """Docstring for test_user_login method."""
+        self.client.post(
+            '/api/v2/auth/signup', data=json.dumps(self.signup_data),
+            content_type='application/json')
+        res2 = self.client.post(
+            '/api/v2/auth/login', data=json.dumps(self.login_data),
+            content_type='application/json')
+        result = json.loads(res2.data)
+        self.assertEqual(result['message'], "successful login")
+        self.assertEqual(res2.status_code, 200)
+
+    def test_create_parcel_order(self):
+        """Docstring for test_create_parcel_order method."""
+        res = self.client.post('/api/v2/parcels',
+                               data=json.dumps(self.parcel_data),
+                               content_type='application/json',
+                               headers=self.user_headers)
+        self.assertEqual(res.status_code, 201)
+
+    def test_valid_username_input(self):
+        """Docstring for test_valid_username method."""
+        self.data = {
+            "email": "fionaityang@gmail.com",
+            "first_name": "Fiona",
+            "last_name": "Murie",
+            "password": "quifi",
+            "username": ""
+        }
+        res = self.client.post(
+            '/api/v2/auth/signup', data=json.dumps(self.data),
+            content_type='application/json')
+        result = json.loads(res.data)
+        self.assertEqual(result['message'], "Please enter a valid username")
+        self.assertEqual(res.status_code, 400)
+
 
 
 if __name__ == '__main__':
